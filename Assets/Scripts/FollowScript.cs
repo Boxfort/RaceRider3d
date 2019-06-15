2ee2b0f337b1f85e71b9c7ac77b027cbf810cdec
Dillon <positiveprogrammer@gmail.com>
2019-06-15 18:27:51 +0100

    Updated post processing effects

---
+++ Assets/Scripts/FollowScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowScript : MonoBehaviour
+{
+    Vector3 offset = new Vector3(0, 20, -10);
+    GameObject car;
+
+    void Start()
+    {
+        car = GameObject.FindGameObjectWithTag("car");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = car.transform.position + offset;
+    }
+}
