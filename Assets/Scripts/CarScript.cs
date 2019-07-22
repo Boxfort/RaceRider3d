61a89e51812e98f5027e34e699a96e934941b65d
Dillon <positiveprogrammer@gmail.com>
2019-07-22 13:56:21 +0100

    Added used car physics, removed non needed folders

--- Assets/Scripts/CarScript.cs
+++ Assets/Scripts/CarScript.cs
@@ -4,19 +4,56 @@ using UnityEngine;
 
 public class CarScript : MonoBehaviour
 {
+    //Adjustable speed for the car forward and turning movement
     public float speed;
 
+    //Right and left front wheels
+    [SerializeField]
+    private GameObject frontRightWheel;
+    [SerializeField]
+    private GameObject frontLeftWheel;
 
+    //Rotation angle of the wheels
+    private float rotangle;
     void Update()
     {
-        GetComponent<Rigidbody>().AddForce(Vector3.forward * speed);
-        if (Input.GetKey(KeyCode.A))
+        //Move the car upward(As the model is already rotated)
+        var v3 = transform.up;
+        v3.y = 0.0f;
+        transform.Translate(v3 * speed * Time.deltaTime, Space.World);
+        //Allinged each Wheel
+        AllignedWheel(frontLeftWheel);
+        AllignedWheel(frontRightWheel);
+
+        //Rotate wheel and set rotangle based on user Input
+        if (Input.GetKey(KeyCode.D) && rotangle <= 180)
         {
-            GetComponent<Rigidbody>().AddForce(Vector3.left * speed);
+            rotangle = rotangle + 40 * Time.deltaTime;
+            frontLeftWheel.transform.Rotate(0, 40 * Time.deltaTime, 0, Space.World);
         }
-        else if(Input.GetKey(KeyCode.D))
+
+        if (Input.GetKey(KeyCode.A) && rotangle >= -180)
         {
-            GetComponent<Rigidbody>().AddForce(Vector3.right * speed);
+            rotangle = rotangle - 40 * Time.deltaTime;
+            frontRightWheel.transform.Rotate(0, -40 * Time.deltaTime, 0, Space.World);//Updated the 40 to -40
         }
+    }
+
+    //Align wheel with the body
+    void AllignedWheel(GameObject wheel)
+    {
+        //Checking whether body is aligned with the wheel
+        if (!Mathf.Approximately(wheel.transform.localEulerAngles.y, 0f))
+        {
+
+            //Since the wheel is attached to the body its rotation in world space will change when body rotates.
+            //To prevent this we maintain the wheel's rotation in world space.
+            Quaternion origWheelRot = wheel.transform.rotation;
+
+            //Rotate body towards wheel's rotation
+            transform.rotation = Quaternion.Lerp(transform.rotation, wheel.transform.rotation, speed);
+            wheel.transform.rotation = origWheelRot;
+        }
+
     }
 }
