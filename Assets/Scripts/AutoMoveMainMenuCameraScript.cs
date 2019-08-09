57c99e73f2ec8d8558cfbf0b6331a6581632f8b5
Dillon <positiveprogrammer@gmail.com>
2019-08-09 11:47:46 +0100

    Added a main menu scene with a ping pong style camera

---
+++ Assets/Scripts/AutoMoveMainMenuCameraScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Camera))]
+public class AutoMoveMainMenuCameraScript : MonoBehaviour
+{
+    //Starting position of camera and end position
+    private Vector3 startingPosition;
+    private Vector3 endPosition;
+    //How much the camera should extend on the z axis
+    [SerializeField]
+    private float cameraExtendZ;
+    //CameraSpeed
+    [SerializeField]
+    private float cameraSpeed;
+    void Start()
+    {
+        //Set starting point
+        startingPosition = transform.position;
+        //Set end point with the extended z axis
+        endPosition = startingPosition;
+        endPosition.z += cameraExtendZ;
+    }
+
+    void FixedUpdate()
+    {
+        //Ping pong between the start and end positions
+        transform.position = Vector3.Lerp(startingPosition, endPosition, Mathf.PingPong(Time.fixedTime* cameraSpeed, 1));
+    }
+}
