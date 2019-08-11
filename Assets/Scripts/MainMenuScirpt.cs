2412ddc780b9430a5bc38694907c9667ec8aed30
Dillon <positiveprogrammer@gmail.com>
2019-08-11 13:52:54 +0100

    .gitignore for unity added, also exit button

--- Assets/Scripts/MainMenuScirpt.cs
+++ Assets/Scripts/MainMenuScirpt.cs
@@ -5,8 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuScirpt : MonoBehaviour
 {
+
+    void Start()
+    {
+
+    }
+
+    //Method for when the start game button is pressed
     public void StartGameButton()
     {
         SceneManager.LoadScene("game");
+    }
+
+    //Method for when the exit game button is pressed
+    public void ExitGameButton()
+    {
+        Application.Quit();
     }
 }
