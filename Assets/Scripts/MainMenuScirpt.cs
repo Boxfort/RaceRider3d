19bf37b32a3462731850c44f27e4dd9e814cebd5
Dillon <positiveprogrammer@gmail.com>
2019-08-11 13:06:39 +0100

    Game scene now load on start button

--- Assets/Scripts/MainMenuScirpt.cs
+++ Assets/Scripts/MainMenuScirpt.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 
 public class MainMenuScirpt : MonoBehaviour
 {
-    
+    public void StartGameButton()
+    {
+        SceneManager.LoadScene("game");
+    }
 }
