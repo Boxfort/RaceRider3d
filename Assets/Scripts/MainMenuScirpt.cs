0f1ede43ecb3fe0476e9ad330ea78b50f75dc3f1
Dillon <positiveprogrammer@gmail.com>
2019-08-12 19:20:54 +0100

    Title screen text, now falls into place

--- Assets/Scripts/MainMenuScirpt.cs
+++ Assets/Scripts/MainMenuScirpt.cs
@@ -5,10 +5,34 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuScirpt : MonoBehaviour
 {
+    //The rect of title text
+    [SerializeField]
+    private RectTransform titleTextTransform;
+    //Where title text should end up
+    [SerializeField]
+    private float endYTitleText;
+    //Starting Position of title text
+    private Vector3 startingPositionTiltleText;
+    //End position of title Text
+    private Vector3 endPositionTitleText;
 
+    //Start method
     void Start()
     {
+        //Set starting position and end position vectors
+        startingPositionTiltleText = titleTextTransform.localPosition;
+        endPositionTitleText = new Vector3(startingPositionTiltleText.x, endYTitleText, startingPositionTiltleText.z);
+        
+    }
 
+    //Update method
+    void FixedUpdate()
+    {
+        //If title text is not at end position lerp it towards it
+       if(titleTextTransform.localPosition.y != endYTitleText)
+       {
+            titleTextTransform.localPosition = Vector3.Lerp(startingPositionTiltleText, endPositionTitleText, 1 * Time.fixedTime);
+       }
     }
 
     //Method for when the start game button is pressed
