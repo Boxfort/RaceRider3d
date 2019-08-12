80f0f237cc0a5d4ac30bb04783287c2b03e2abb4
Dillon <positiveprogrammer@gmail.com>
2019-08-12 20:29:18 +0100

    Restart button now works

--- Assets/Scripts/GenScript.cs
+++ Assets/Scripts/GenScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GenScript : MonoBehaviour
 {
@@ -171,6 +172,11 @@ public class GenScript : MonoBehaviour
 	{
 		endGameCanvas.SetActive(true);
 		gameEnded = true;
+	}
+
+	public void RestartGameButton()
+	{
+		SceneManager.LoadScene("game");
 	}
 }
 
