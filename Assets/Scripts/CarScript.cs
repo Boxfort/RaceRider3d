c4468ea4587c54ef58d4070e981dde01f925df3f
Dillon <positiveprogrammer@gmail.com>
2019-08-12 20:26:23 +0100

    Fixed car not stopping after end game

--- Assets/Scripts/CarScript.cs
+++ Assets/Scripts/CarScript.cs
@@ -13,6 +13,8 @@ public class CarScript : MonoBehaviour
     private float orginalZRotation;
     //Store car rotation
     private float rotationDirection;
+	//Is car moving
+	private bool isCarMoving = true;
 
 
     void Start()
@@ -22,26 +24,29 @@ public class CarScript : MonoBehaviour
     }
     void FixedUpdate()
     {
-        var v3 = transform.up;
-        v3.y = 0.0f;
-        //Move the car upward(As the model is already rotated)
-        transform.Translate(v3 * Time.fixedDeltaTime * speed, Space.World);
+		if(isCarMoving)
+		{
+			Vector3 v3 = transform.up;
+			v3.y = 0.0f;
+			//Move the car upward(As the model is already rotated)
+			transform.Translate(v3 * Time.fixedDeltaTime * speed, Space.World);
 
-        //Move car left or right based on phone tilt
-        if (Input.acceleration.normalized.x > tiltFactor)
-        {
-            transform.Rotate(0f, 0f, 1f);
-            rotationDirection = 1f;
-        }
-        else if (Input.acceleration.normalized.x <= -tiltFactor)
-        {
-            transform.Rotate(0f, 0f, -1f);
-            rotationDirection = -1f;
-        }
-        else if(!Mathf.Approximately(orginalZRotation,transform.rotation.z))
-        {
-            transform.Rotate(0f, 0f, rotationDirection);
-        }
+			//Move car left or right based on phone tilt
+			if (Input.acceleration.normalized.x > tiltFactor)
+			{
+				transform.Rotate(0f, 0f, 1f);
+				rotationDirection = 1f;
+			}
+			else if (Input.acceleration.normalized.x <= -tiltFactor)
+			{
+				transform.Rotate(0f, 0f, -1f);
+				rotationDirection = -1f;
+			}
+			else if (!Mathf.Approximately(orginalZRotation, transform.rotation.z))
+			{
+				transform.Rotate(0f, 0f, rotationDirection);
+			}
+		}
     }
 
 	//Car collision method
@@ -51,6 +56,7 @@ public class CarScript : MonoBehaviour
         if(collision.gameObject.tag == "Object")
         {
 			GameObject.Find("GameManger").GetComponent<GenScript>().EndGameScreenDisplay();
+			isCarMoving = false;
         }
     }
 }
