95aee761e0c593db1676f8e333c310e8779ac64a
Dillon <positiveprogrammer@gmail.com>
2019-08-16 11:49:05 +0100

    CustomiseScript created and camera car render texture for the ui

---
+++ Assets/Scripts/CustomiseScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomiseScript : MonoBehaviour
+{
+	[SerializeField]
+	private Transform cameraCarRotationTransform;
+	[SerializeField]
+	private GameObject normalCar;
+	[SerializeField]
+	private GameObject suvCar;
+	[SerializeField]
+	private GameObject sportsCar;
+	[SerializeField]
+	private GameObject copCar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	public void ForwardButton()
+	{
+
+	}
+
+	public void BackwardsButton()
+	{
+
+	}
+}
